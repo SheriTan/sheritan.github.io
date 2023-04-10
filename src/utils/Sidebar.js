6b0f51bf0cc9792e248b795d3bf89f0bb5f5f339
SheriTan <sheritan.20@ichat.sp.edu.sg>
2023-04-11 06:22:02 +0800

    sidebar setup

--- src/utils/Sidebar.js
+++ src/utils/Sidebar.js
@@ -1,5 +1,22 @@
 import React, { useEffect, useRef, useState } from 'react';
 const Sidebar = () => {
+    const sidebarItems = [
+        {
+            card: 'About',
+            to: '/about',
+            section: 'about'
+        },
+        {
+            card: 'Projects',
+            to: '/projects',
+            section: 'projects'
+        },
+        {
+            card: 'Contact',
+            to: '/contact',
+            section: 'contact'
+        },
+    ]
     return (
         <>
         </>
